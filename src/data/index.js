8a78b7bdce36ae56ecd080be89bfbc7420482603
thanwin84 <thanwinhline84@gmail.com>
2025-01-27 20:00:00 +0600

    Add 'thanwin' as a contributor to multiple projects

--- src/data/index.js
+++ src/data/index.js
@@ -3,24 +3,24 @@ export const projects = [
     id: 'counter-app',
     name: 'Counter App',
     description: 'A simple counter app.',
-    contributors: ['bongoDev', 'talha', 'nazma', 'abu taher'],
+    contributors: ['bongoDev', 'talha', 'nazma', 'abu taher', 'thanwin'],
   },
   {
     id: 'number-comparator',
     name: 'Number Comparator',
     description: 'A simple number comparator app.',
-    contributors: ['bongoDev', 'shobuz', 'sumaiya'],
+    contributors: ['bongoDev', 'shobuz', 'sumaiya', 'thanwin'],
   },
   {
     id: 'grade-calculator',
     name: 'Grade Calculator',
     description: 'A simple grade calculator app.',
-    contributors: [],
+    contributors: ['thanwin'],
   },
   {
     id: 'render-products',
     name: 'Render Products',
     description: 'A simple product rendering app using useEffect hook.',
-    contributors: ['bongoDev'],
+    contributors: ['bongoDev', 'thanwin'],
   },
 ];
